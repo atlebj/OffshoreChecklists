4cca9d1404f24f7e5512dd973e98b4ebeb1370f0
Atle Bj <atlebjornevoll@gmail.com>
2023-11-15 11:41:10 +0100

    ok

--- read-test.js
+++ read-test.js
@@ -1,7 +1,6 @@
 const { MongoClient } = require('mongodb');
 
 async function main() {
-  const uri = "mongodb://checklistsapp:pxj3skdkxarK9dxT4en4gA0HwideX9HQbeFq0NkVbPINvFv4GBv9d4Hnug2AnWwwc5b5WECJGUrtACDbV9ijyg%3D%3D@checklistsapp.mongo.cosmos.azure.com:10255/checklistapp?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@checklistsapp@"; // Replace with your MongoDB connection string
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
   try {
